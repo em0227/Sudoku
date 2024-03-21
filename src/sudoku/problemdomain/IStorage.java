9779743750874aaff2f7bfdec993315c53965056
Emily Wu <emily.wu@aspencapital.com>
2024-03-21 19:47:42 -0400

    feat: add constants

---
+++ src/sudoku/problemdomain/IStorage.java
@@ -0,0 +1,8 @@
+package sudoku.problemdomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameData(SudukoGame game) throws IOException;
+    SudukoGame getGameData() throws IOException;
+}
